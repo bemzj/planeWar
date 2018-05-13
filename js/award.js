e039057f874479406c78720324be7ae96f13eac5
Your Name <email@example.com>
2018-05-13 18:44:21 +0800

    xx

---
+++ js/award.js
@@ -0,0 +1,90 @@
+$(function(){
+	var turnplate={
+		startAngle:0,				//开始角度
+		randomRate:[],              //控制获奖率，百分制(相加需等于100%)，对应restaraunts(顺序需要保持一致)，
+		bRotate:false				//false:停止;ture:旋转
+	};
+	var sum;
+	turnplate.randomRate = ["0%", '0%', '0%', '0%', '0%', '0%', '0%', '0%'];
+	//设置turnplate.randomRate的数组长度就是大转盘格子的个数
+	var rotateTimeOut = function (){
+		$('.awardGift').rotate({
+			angle:0,
+			animateTo:2160,
+			duration:8000,
+			callback:function (){
+				alert('网络超时，请检查您的网络设置！');
+			}
+		});
+	};
+	//旋转
+	var rotateFn = function (item, txt){
+		var angles = item * (360 / turnplate.randomRate.length) - (360 / (turnplate.randomRate.length*2));
+		if(angles<270){
+			angles = 270 - angles; 
+		}else{
+			angles = 360 - angles + 270;
+		}
+		$('.awardGift').stopRotate();
+		$('.awardGift').rotate({
+			angle:0,
+			animateTo:angles+1800,
+			duration:8000,
+			callback:function (){
+				
+				//旋转完成调用的函数
+				alert(txt.giftCode);
+				//开启旋转
+				turnplate.bRotate = !turnplate.bRotate;
+			}
+		});
+	};
+	//点击开始
+	$('.start').click(function (){
+		
+		if(turnplate.bRotate)return;
+		turnplate.bRotate = !turnplate.bRotate;
+
+		
+
+		$.get('json/award.json',function(data){
+			turnplate.randomRate = ["0%", '0%', '0%', '0%', '0%', '0%', '0%', '0%'];
+			//从后台请求获取数据设置对应的数组为100%
+			
+			turnplate.randomRate[data.giftCode] = "100%";
+			//获取随机数(奖品个数范围内)
+			var item = rnd(turnplate.randomRate);
+			rotateFn(item,data);//data为你要带的参数
+		});
+		
+	});
+	//注意如果转盘对不准的情况下。可以调节awardGift下的img的旋转角度进去调节transform: rotate(22deg);。
+});
+function rnd(rate){
+	var random = Math.floor(Math.random() * 100);
+	var myRandom = [];
+	var randomList = [];
+	var randomParent = [];
+	for(var i = 0; i < 100; i++){
+		myRandom.push(parseInt([i]) + 1);
+	}
+	for(var i = 0; i < rate.length; i++){
+		var temp = [];
+		var start = 0;
+		var end = 0;
+		randomList.push(parseInt(rate[i].split('%')[0]));
+		for(var j = 0; j < randomList.length; j++){
+			start += randomList[j-1] || 0
+			end += randomList[j]
+		}
+		temp = myRandom.slice(start, end);
+		randomParent.push(temp)
+	}
+	for(var i = 0; i < randomParent.length; i++){
+		if($.inArray(random, randomParent[i]) > 0){
+			return(i+1)
+		}
+	}
+	
+}
+
--- js/popWin.js
+++ js/popWin.js
@@ -53,3 +53,110 @@ function showPop(id,txt,fun) {
         },900);
     });
 }
+//游戏成功
+function gameSuccess(score){
+	$('#success').remove();
+	var html = "";
+	html += '<div id="success" class="pw mask"><div class="success1 animated bounceInDown">';
+	html += '<img src="img/success1.png" class="img"/></div><div class="success2 animated fadeInDown">';
+	html += '<img src="img/success2.png" class="img"  /><div class="success21"><img src="img/success3.png" class="img" />';
+	html += '</div><div class="success22"><img src="img/success4.png" class="img" />';
+	html += '</div><div class="success23"><img src="img/success2.png" class="img" />';
+	html += '</div></div><div class="success3 animated bounceIn">';
+	html += '<p>获得'+score+'积分与1次抽奖机会<br/>捍卫家园，消灭空气杀手<br/>你是位出色的除醛战士</p>';
+	html += '</div><div class="success4 animated fadeInUp"><a href="###"><img src="img/success5.png"/></a></div></div>';
+	$('body').append(html);
+	$('#success .success4 a').one('touchstart',function(){
+		
+		app.pageIndex = 6;		
+		$('#success').remove();
+	});
+}
+//游戏失败
+function gameFail(score,src){
+	$('#fail').remove();
+	var html = "";
+	html +='<div id="fail" class="pw mask" ><div class="fail1 animated bounceInDown">';
+	html +='<img src="img/error1.png" class="img"/></div>';
+	html +='<div class="fail2 animated fadeInDown"><img src="img/error2.png" class="img" /></div>';
+	html +='<div class="fail3 animated bounceIn">';
+	html +='<p>获得'+score+'积分，成为除醛战士<br/>不是一朝一夕，再接再励！</p></div>';
+	html +='<div class="fail4 clearBoth"><a href="###" class="floatl  animated fadeInLeft"><img src="img/error4.png"/> </a>';
+	html +='<a href="###" class="floatr  animated fadeInRight"><img src="img/error3.png"/> </a>';
+	html +='</div><div class="fail5 animated fadeInUp"><a href="###"><img src="img/error5.png"/></a></div></div>';
+	$('body').append(html);
+	//返回首页
+	$('.fail4 .floatl').one('touchstart',function(){
+		$('#fail').remove();
+		app.pageIndex = 1;
+	});
+	//再次挑战
+	$('.fail4 .floatr').one('touchstart',function(){
+
+	});
+	//链接
+	$('.fail5 a').one('touchstart',function(){
+		window.location.href = src;
+	});
+}
+//激活失败
+function shareFail(){
+	$('#fail').remove();
+	var html = "";
+	html +='<div id="fail" class="pw mask fail" ><div><div class="fail1 animated bounceInDown">';
+	html +='<img src="img/share1.png" class="img"/></div>';
+	html +='<div class="fail2 animated fadeInDown"><img src="img/error2.png" class="img" /></div>';
+	html +='<div class="fail3 animated bounceIn">';
+	html +='<p>请重新分享朋友圈</p></div>';
+	html +='<div class="fail6 animated fadeInUp"><a href="###"><img src="img/share2.png"/></a></div></div></div>';
+	$('body').append(html);
+	//返回首页
+	$('.fail6 a').one('touchstart',function(){
+		
+		$('#fail>div').addClass('animated bounceOut');
+		setTimeout(function(){
+			$('#fail').remove();
+			app.pageIndex = 1;	
+		},900);
+	});
+}
+//激活失败
+function shareSuccess(){
+	$('#success').remove();
+	var html = "";
+	html += '<div id="success" class="pw mask success"><div><div class="success1 animated bounceInDown">';
+	html += '<img src="img/share3.png" class="img"/></div><div class="success2 animated fadeInDown">';
+	html += '<img src="img/success2.png" class="img"  /><div class="success21"><img src="img/success3.png" class="img" />';
+	html += '</div><div class="success22"><img src="img/success4.png" class="img" />';
+	html += '</div><div class="success23"><img src="img/success2.png" class="img" />';
+	html += '</div></div><div class="success3 animated bounceIn">';
+	html += '<p>礼品已放置“我的礼品，<br/>填写个人信息即可领取。</p>';
+	html += '</div><div class="success4 animated fadeInUp"><a href="###"><img src="img/share2.png"/></a></div></div></div>';
+	$('body').append(html);
+	$('#success .success4 a').one('touchstart',function(){
+		$('#success>div').addClass('animated bounceOut');
+		setTimeout(function(){
+			$('#success').remove();
+			app.pageIndex = 2;	
+		},900);	
+	});
+}
+//获得礼物
+function getGameGift(){
+	$('#giftPop').remove();
+	var html = "";
+	html += '<div id="giftPop" class="pw mask"><div class="giftPop animated bounceInDown">';
+	html += '<img src="img/getGift.png" class="img"/><div class="giftPopIn">';
+	html += '<div class="gp1"><img src="img/gift5.png" /></div><div class="gp2">';
+	html += '<p>免费获得一个</p><p>价值XX元的抱枕</p></div><div class="gp3">';
+	html += '<p>马上分享朋友圈，激活领取礼品！</p></div><div class="gp4">';
+	html += '<a href="###"><img src="img/share2.png"/></a></div></div></div></div>';
+	$('body').append(html);
+	$('.gp4 a').on('touchstart',function(){
+		$('.giftPop').addClass('animated bounceOut');
+		setTimeout(function(){
+			$('#giftPop').remove();
+			app.pageIndex = 2;	
+		},900);
+	});
+}
